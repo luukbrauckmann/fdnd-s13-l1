15e6575303228afd635cefddb16705cf65cfcbaa
Luuk <luukbrauckmann@gmail.com>
2023-09-05 18:21:37 +0200

    Update - Gsap toegevoegd - Schilderijen toegevoegd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,4 +7,18 @@
 
 <img class="light" src="light.svg" alt>
 
-<h1>{ page.title }</h1>
+<div class="content">
+	<article>
+		<header>
+			<h2>Foto</h2>
+		</header>
+
+	</article>
+
+	<article>
+		<header>
+			<h2>Text</h2>
+		</header>
+
+	</article>
+</div>
